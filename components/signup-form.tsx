04ab2ab6338fe372e8a45e6d8f627e8f36395f80
shiftmg <flyordig@shift.co.kr>
2024-04-05 11:54:39 +0900

    회원가입 취소 버튼 추가

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -62,6 +62,10 @@ export default function LoginForm() {
             router.push("/");
         }
     },[join])
+    const toLoginForm = () => {
+        location.href = "/";
+
+    }
 
     return (<>
             <form onSubmit={onChangeSignUpHandler}>
@@ -77,6 +81,7 @@ export default function LoginForm() {
                     <input type="password" name="password2" id="password2" />
 
                     <input className={styles.signupBtn} type="submit" value="Save" />
+                    <input className={styles.toLoginFormBtn} type="button" value="취소" onClick={toLoginForm}/>
                 </fieldset>
             </form>
             </>);
